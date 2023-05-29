ff6adb18042a7050abc9a3483bfa276b5832f7e8
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-29 19:42:16 +0300

    Add basic import and export examples for commonJS

---
+++ MyMath.js
@@ -0,0 +1,25 @@
+const a = 5;
+const b = 10;
+
+const c = a * b;
+
+const data = null;
+
+class MyMath {
+  static sum = (...numbers) => {
+    let result = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+      result += numbers[i];
+    }
+
+    return result;
+  };
+
+  static divide = (...numbers) =>
+    numbers.reduce((acc, currentNumber) => acc / currentNumber);
+}
+
+// експорт даних
+exports.MyMath = MyMath;
+exports.test = 'це воно';
--- index.js
+++ index.js
@@ -1,30 +1,9 @@
 'use strict';
+// імпорт модулів
+const exportsObj = require('./MyMath.js');
+const { MyMath, test } = exportsObj;
 
-const a = 5;
-const b = 10;
-
-const c = a * b;
-
-class MyMath {
-  static sum = (...numbers) => {
-    let result = 0;
-
-    for (let i = 0; i < numbers.length; i++) {
-      result += numbers[i];
-    }
-
-    return result;
-  };
-
-  static divide = (...numbers) =>
-    numbers.reduce((acc, currentNumber) => acc / currentNumber);
-}
-
-class Component {
-  render() {
-    return `rendered some component with data`;
-  }
-}
+const data = 12345;
 
 console.log(MyMath.sum(2, 2, 3, -1));
 
